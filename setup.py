8de23409db00fc2ed4ad806bf858b5e6970ea389
唐大勇 <tangdyy@qq.com>
2020-03-27 21:40:49 +0800

    feat: 增加发布文件setup.py
    Signed-off-by: 唐大勇 <tangdyy@qq.com>

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages 
+
+setup(
+    name = 'onlyuserclient',
+    version = '1.0.0',
+    description = 'onlyuser client',
+    author = 'Tang dayong',    
+    requires=['django', 'djangorestframework'],
+    package_dir={'': 'src'},
+    packages=['onlyuserclient']
+)
