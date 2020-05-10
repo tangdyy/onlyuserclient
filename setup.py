b9aef676b73890e82d43f50c730508d4bc675797
唐大勇 <tangdyy@qq.com>
2020-05-10 16:58:58 +0800

    fix: 修改setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'onlyuserclient',
-    version = '1.0.3',
+    version = '1.0.4',
     description = 'onlyuser client',
     author = 'Tang dayong',    
     requires=['django', 'djangorestframework'],
--- src/onlyuserclient/__init__.py
+++ src/onlyuserclient/__init__.py
@@ -1,6 +1,6 @@
 '''onlyuser 客户端工具包
 '''
 
-__version__='1.0.3'
+__version__='1.0.4'
 
 VERSION = __version__
