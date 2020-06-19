7b82e7d8d5d47e9497c5899914f56d83ef50a947
唐大勇 <674822668@qq.com>
2020-06-19 11:28:03 +0800

    fix: 修改版本号
    Signed-off-by: 唐大勇 <674822668@qq.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'onlyuserclient',
-    version = '1.0.4',
+    version = '1.0.5',
     description = 'onlyuser client',
     author = 'Tang dayong',    
     requires=['django', 'djangorestframework'],
