fa8beeb7ea18ad1cc391d591f474c62a7b443149
唐大勇 <tangdyy@qq.com>
2021-10-16 15:33:28 +0800

    fix: 修改计费

--- src/onlyuserclient/middleware/billing.py
+++ src/onlyuserclient/middleware/billing.py
@@ -4,7 +4,7 @@ class BillingMiddleware():
     '''计费控制中间建
     '''
     def __init__(self, get_response):
-        print('this middleware __init__ ')
+
         self.get_response = get_response
 
     def get_user_id(self, request=None):
@@ -12,16 +12,8 @@ class BillingMiddleware():
 
 
     def __call__(self, request):
-        print('this get_response before.')
         response = self.get_response(request)
-        print('this get_response after.')
         return response 
     
     def process_view(self, request, view_func, view_args, view_kwargs):
-        print(view_func.__name__)
-        print(dir(view_func))
-        #print(view_func.cls, '\n') 
-        #print(view_func.actions, '\n')
-        #print(dir(view_func.cls), '\n')
-        print(view_args, view_kwargs, '\n')
         return None
