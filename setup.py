2497e4e50fc0cc0874bfa9c8981aae060274d17c
tangdy <2012830018@qq.com>
2021-11-08 13:10:35 +0800

    fix: 修改版本号

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r", encoding='utf8') as fh:
 
 setup(
     name = 'onlyuserclient',
-    version = '1.1.0.dev4',
+    version = '1.1.0.dev5',
     description = 'onlyuser client',
     long_description=long_description,
     long_description_content_type="text/markdown",
