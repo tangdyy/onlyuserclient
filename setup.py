56e12f5984b896e1e6b1204b10de446ebe30721f
tangdy <2012830018@qq.com>
2023-08-07 16:51:19 +0800

    feat: 发布1.2.8

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("readme.md", "r", encoding='utf8') as fh:
 
 setup(
     name = 'onlyuserclient',
-    version = '1.2.7',
+    version = '1.2.8',
     description = 'onlyuser client',
     long_description=long_description,
     long_description_content_type="text/markdown",
