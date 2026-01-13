c03e2bdc40bff1bc8a00410b84b10553329b55af
tangdy <2012830018@qq.com>
2026-01-13 17:01:05 +0800

    fix: 修改版本号

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("readme.md", "r", encoding='utf8') as fh:
 
 setup(
     name = 'onlyuserclient',
-    version = '1.3.0',
+    version = '1.3.1.dev0',
     description = 'onlyuser client',
     long_description=long_description,
     long_description_content_type="text/markdown",
--- src/onlyuserclient/__init__.py
+++ src/onlyuserclient/__init__.py
@@ -1,6 +1,6 @@
 '''onlyuser 客户端开发包
 '''
 
-__version__='1.3.0'
+__version__='1.3.1.dev0'
 
 VERSION = __version__
