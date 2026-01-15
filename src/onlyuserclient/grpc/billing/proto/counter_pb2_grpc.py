d81c9d97737a6236e0042072b36b30dfca9039e9
tangdy <2012830018@qq.com>
2026-01-15 17:11:23 +0800

    fix: 修改grpc

--- src/onlyuserclient/grpc/billing/proto/counter_pb2_grpc.py
+++ src/onlyuserclient/grpc/billing/proto/counter_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import counter_pb2 as counter__pb2
+from . import counter_pb2 as counter__pb2
 
 
 class CounterServiceStub(object):
